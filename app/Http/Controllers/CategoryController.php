47c015e6f21a81b8d3d5ee75b585caf11a470f6f
joseCnarciso <josecdia@hotmail.com>
2023-11-18 16:05:22 -0300

    Exercicio numero 6 concluido

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use Exception;
+use Illuminate\Validation\Rule;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    public function index()
+    {
+        $categories = Category::all();
+        return $categories;
+    }
+    public function store(Request $request)
+    {
+        try {
+            $request->validate([
+                "name" => 'required|unique:categories|string|max:150'
+            ]);
+
+            $data = $request->all();
+            $category = Category::create($data);
+            return $category;
+        } catch (Exception $exception) {
+            return response()->json(['message' => $exception->getMessage()], 400);
+        }
+    }
+    public function show($id)
+    {
+        $category = Category::find($id);
+        if (!$category) return response()->json(['message' => 'Categoria não encontrado'], 404);
+        return $category;
+    }
+
+    public function update($id, Request $request)
+    {
+        try {
+            $category = Category::find($id);
+
+            if (!$category) return response()->json(['message' => 'Categoria não encontrado', 404]);
+            $request->validate([
+                'name' => [
+                    'required',
+                    Rule::unique('products')->ignore($category->$id),
+                    ]
+            ]);
+            $category->update($request->all());
+            return $category;
+        } catch (Exception $exception) {
+            return response()->json(['message' => $exception->getMessage()], 400);
+        }
+    }
+    public function destroy(string $id)
+    {
+        $category = Category::find($id);
+        if (!$category) return response()->json(['message' => 'Categoria não encontrado', 404]);
+        $category->delete();
+        return response('',204);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,10 @@
 <?php
+use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 
-// implemente sua rota;
+Route::get('categorys', [CategoryController::class, 'index']);
+Route::post('categorys', [CategoryController::class, 'store']);
+Route::get('categorys/{id}', [CategoryController::class, 'show']);
+Route::delete('categorys/{id}', [CategoryController::class, 'destroy']);
+Route::put('categorys/{id}', [CategoryController::class, 'update']);
 
