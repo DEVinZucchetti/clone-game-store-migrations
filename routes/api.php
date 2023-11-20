bebe58c7c1b0b07d5ef64f85bf73b86861dc8a6b
lcsrlm <lucas_rosa-lima@estudante.sesisenai.org.br>
2023-11-20 18:04:48 -0300

    cria rotas

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,11 @@
 <?php
+
+use App\Http\Controllers\AvaliationController;
 use Illuminate\Support\Facades\Route;
 
 // implemente sua rota;
 
+Route::get('avaliations',[AvaliationController::class, 'index']);
+Route::post('avaliations',[AvaliationController::class, 'store']);
+Route::put('avaliations',[AvaliationController::class, 'update']);
+Route::delete('avaliations',[AvaliationController::class, 'destroy']);
